a527a7322beeb96a508cd4d11ed8bdac57700d14
fikrighifarim <vinalesnow@gmail.com>
2022-01-10 17:48:20 +0700

    Add profile header

---
+++ src/assets/Dummy/index.js
@@ -0,0 +1,5 @@
+import ProfileDummy from './profile.png'
+
+export {
+    ProfileDummy
+}
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,44 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
+import { ProfileDummy } from '../../assets'
 
 const Home = () => {
     return (
         <View>
-            <Text>Home Page</Text>
+            <View style={styles.profileContainer}>
+                <View>
+                    <Text style={styles.Title}>Warung Jeha</Text>
+                    <Text style={styles.subTitle}>Selamat berbelanja</Text>
+                </View>
+                <Image source={ProfileDummy} style={styles.profile}/>
+            </View>
         </View>
     )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    profileContainer: {
+        flexDirection:'row',
+        justifyContent:'space-between',
+        paddingTop: 32,
+        paddingHorizontal: 24,
+        backgroundColor: 'white'
+    },
+    profile :{
+        width:50,
+        height:50,
+        borderRadius: 8,
+    },
+    Title :{
+        fontFamily: 'Poppins-Medium',
+        fontSize: 22,
+        color: '#020202',
+    },
+    subTitle:{
+        fontFamily: 'Poppins-Light',
+        fontSize: 14,
+        color: '#8D92A3', 
+    }
+})
