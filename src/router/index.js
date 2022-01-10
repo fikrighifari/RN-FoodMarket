8a176cdf4ac5e25e533981fc14afbacfca5ffca9
fikrighifarim <vinalesnow@gmail.com>
2022-01-10 15:32:40 +0700

    Add tab navigator

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,37 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
-import {SignIn, SignUp, SignUpAddress, SplashScreen, SuccessSignUp} from '../pages';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {
+  Home,
+  Order,
+  Profile,
+  SignIn,
+  SignUp,
+  SignUpAddress,
+  SplashScreen,
+  SuccessSignUp,
+} from '../pages';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const MainApp = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={Home} options={{headerShown: false}} />
+      <Tab.Screen
+        name="Order"
+        component={Order}
+        options={{headerShown: false}}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{headerShown: false}}
+      />
+    </Tab.Navigator>
+  );
+};
 
 const Router = () => {
   return (
@@ -30,6 +59,11 @@ const Router = () => {
       <Stack.Screen
         name="SuccessSignUp"
         component={SuccessSignUp}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="MainApp"
+        component={MainApp}
         options={{headerShown: false}}
       />
     </Stack.Navigator>
