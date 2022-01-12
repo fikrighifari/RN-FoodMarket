3cf1e604cfa55b01d053c0fccc1af0170f502a7c
fikrighifarim <vinalesnow@gmail.com>
2022-01-12 14:21:12 +0700

    add counter button in detail page

--- src/components/molecules/Counter/index.js
+++ src/components/molecules/Counter/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {IcAdd, IcMin} from '../../../assets';
 
 const Counter = () => {
   return (
-    <View>
-      <Text style={styles.labelCounter}>Counter</Text>
+    <View style={styles.container}>
+      <TouchableOpacity activeOpacity={0.3}>
+        <IcMin />
+      </TouchableOpacity>
+      <Text style={styles.value}>14</Text>
+      <TouchableOpacity activeOpacity={0.3}>
+        <IcAdd />
+      </TouchableOpacity>
     </View>
   );
 };
@@ -12,9 +19,14 @@ const Counter = () => {
 export default Counter;
 
 const styles = StyleSheet.create({
-  labelCounter: {
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  value: {
     fontFamily: 'Poppins-Regular',
     fontSize: 16,
     color: '#020202',
+    marginHorizontal: 16,
   },
 });
