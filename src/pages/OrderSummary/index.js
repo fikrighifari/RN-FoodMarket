f1560a9aef2b907f2f3c8c651e235e96804466c3
fikrighifarim <vinalesnow@gmail.com>
2022-01-12 14:27:58 +0700

    add base order summary page

---
+++ src/pages/OrderSummary/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const OrderSummary = () => {
+    return (
+        <View>
+            <Text>Order Summay Page</Text>
+        </View>
+    )
+}
+
+export default OrderSummary
+
+const styles = StyleSheet.create({})
